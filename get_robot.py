a953c8c4106d40d98f7d7785dcc180bf043bd624
AndreSchakkal <andre.schakkal@gmail.com>
2023-03-13 22:15:06 +0100

    2_robots

--- get_robot.py
+++ get_robot.py
@@ -13,7 +13,9 @@ class sim_robot_env:
         plane = p.loadURDF("plane_transparent.urdf")
         p.changeVisualShape(plane, -1, rgbaColor=[0, 0, 0, 0])
         startPos = [0, 0, 0]
+        startPos2 = [0,0.8,0]
         startOrientation = p.getQuaternionFromEuler([0, 0, 0])
+        startOrientation2 = p.getQuaternionFromEuler([0, 0, 0])
         p.setGravity(0, 0, -9.81)
         p.setTimeStep(0.001)
         p.setRealTimeSimulation(use_sim)
@@ -21,15 +23,15 @@ class sim_robot_env:
                                             cameraTargetPosition=[0, 0, 0])
 
         self.robot = p.loadURDF("kuka_iiwa/model.urdf", startPos, startOrientation, useFixedBase=1)
-
+        self.robot2 = p.loadURDF("kuka_iiwa/model.urdf", startPos2, startOrientation2, useFixedBase=1)
         '''
         Create a scene too
         the environment with the box and table for the different scenarios I would like in the simulation
         '''
-        self.box = p.loadURDF("../descriptions/robot_descriptions/objects_description/objects/simple_box.urdf",
+        self.box = p.loadURDF("descriptions/robot_descriptions/objects_description/objects/simple_box.urdf",
                               [0.5, 0.3, 0.5], globalScaling=1.0, useFixedBase=0)
         tableOrientation = p.getQuaternionFromEuler([0, 0, math.pi / 2])
-        self.table = p.loadURDF("../descriptions/robot_descriptions/objects_description/objects/table.urdf",
+        self.table = p.loadURDF("descriptions/robot_descriptions/objects_description/objects/table.urdf",
                            [1.15, 0.45, 0.0], tableOrientation, globalScaling=1.0, useFixedBase=1)
         p.changeDynamics(self.box, -1, mass=box_object.mass, linearDamping=0.04, angularDamping=0.04, rollingFriction=0.01,
                          spinningFriction=0.02, restitution=0, lateralFriction=0.1)
@@ -37,8 +39,9 @@ class sim_robot_env:
                          spinningFriction=0.02, restitution=0, lateralFriction=0.1)
 
         
+        # What is all that?
         self.numJoints = p.getNumJoints(self.robot)
-        self.ee_id = 6
+        self.ee_id = 6              #ee: end effector
         self.zeros = [0.0] * 7
         self.relative_ee = [0, 0, 0]
 
@@ -54,14 +57,24 @@ class sim_robot_env:
 
     def get_IK_joint_position(self, x):
         return p.calculateInverseKinematics(self.robot, self.ee_id, x)
+    def get_IK_joint_position2(self, x):
+        return p.calculateInverseKinematics(self.robot2, self.ee_id, x)
+
 
     def set_to_joint_position(self, q):
         for i in range(self.numJoints):
             p.resetJointState(self.robot, i, q[i])
+    def set_to_joint_position2(self, q):
+        for i in range(self.numJoints):
+            p.resetJointState(self.robot2, i, q[i])
 
     def move_with_joint_velocities(self, q_dot):
         p.stepSimulation()
         p.setJointMotorControlArray(self.robot, range(self.numJoints), controlMode=p.VELOCITY_CONTROL,
+                               targetVelocities=q_dot.tolist())
+    def move_with_joint_velocities2(self, q_dot):
+        p.stepSimulation()
+        p.setJointMotorControlArray(self.robot2, range(self.numJoints), controlMode=p.VELOCITY_CONTROL,
                                targetVelocities=q_dot.tolist())
 
     def move_to_joint_position(self, q, q_dot):
@@ -73,43 +86,83 @@ class sim_robot_env:
                                     force=500,
                                     positionGain=1,
                                     velocityGain=1)
+    def move_to_joint_position2(self, q, q_dot):
+        p.stepSimulation()
+        for i in range(self.numJoints):
+            p.setJointMotorControl2(self.robot2, jointIndex=i, controlMode=p.POSITION_CONTROL,
+                                    targetPosition=q[i],
+                                    targetVelocity=q_dot[i],
+                                    force=500,
+                                    positionGain=1,
+                                    velocityGain=1)
 
     def get_joint_position(self):
         joint_state = p.getJointStates(self.robot, range(self.numJoints))
         joint_position = [state[0] for state in joint_state]
         return joint_position
+    
+    def get_joint_position2(self):
+        joint_state = p.getJointStates(self.robot2, range(self.numJoints))
+        joint_position = [state[0] for state in joint_state]
+        return joint_position
 
     def get_ee_position(self):
         return p.getLinkState(self.robot, self.ee_id)[0]
+    
+    def get_ee_position2(self):
+        return p.getLinkState(self.robot2, self.ee_id)[0]
 
     def get_trans_jacobian(self):
         q = self.get_joint_position()
         jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot, self.ee_id, self.relative_ee, q, self.zeros, self.zeros)
         return jac_t_fn
-
+    def get_trans_jacobian2(self):
+        q = self.get_joint_position2()
+        jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot2, self.ee_id, self.relative_ee, q, self.zeros, self.zeros)
+        return jac_t_fn
+    
     def get_rot_jacobian(self):
         q = self.get_joint_position()
         jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot, self.ee_id, self.relative_ee, q, self.zeros, self.zeros)
+        return jac_r_fn
+    def get_rot_jacobian2(self):
+        q = self.get_joint_position()
+        jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot2, self.ee_id, self.relative_ee, q, self.zeros, self.zeros)
         return jac_r_fn
 
     def get_trans_jacobian_specific(self, q_specific):
         jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot, self.ee_id, self.relative_ee, q_specific, self.zeros, self.zeros)
         return jac_t_fn
+    def get_trans_jacobian_specific2(self, q_specific):
+        jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot2, self.ee_id, self.relative_ee, q_specific, self.zeros, self.zeros)
+        return jac_t_fn
 
     def get_rot_jacobian_specific(self, q_specific):
         jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot, self.ee_id, self.relative_ee, q_specific, self.zeros, self.zeros)
         return jac_r_fn
+    def get_rot_jacobian_specific2(self, q_specific):
+        jac_t_fn, jac_r_fn = p.calculateJacobian(self.robot2, self.ee_id, self.relative_ee, q_specific, self.zeros, self.zeros)
+        return jac_r_fn
 
     def get_mass_matrix_specific(self, q):
         return p.calculateMassMatrix(self.robot, q)
+    def get_mass_matrix_specific2(self, q):
+        return p.calculateMassMatrix(self.robot2, q)
 
     def get_mass_matrix(self):
         q = self.get_joint_position()
         return p.calculateMassMatrix(self.robot, q)
+    def get_mass_matrix2(self):
+        q = self.get_joint_position()
+        return p.calculateMassMatrix(self.robot2, q)
 
     def get_inertia_matrix_specific(self, q):
         J = self.get_trans_jacobian_specific(q)
         M = self.get_mass_matrix_specific(q)
+        return np.array(np.linalg.inv(np.array(J) @ np.linalg.inv(np.array(M)) @ np.array(J).T))
+    def get_inertia_matrix_specific2(self, q):
+        J = self.get_trans_jacobian_specific2(q)
+        M = self.get_mass_matrix_specific2(q)
         return np.array(np.linalg.inv(np.array(J) @ np.linalg.inv(np.array(M)) @ np.array(J).T))
 
     def get_inertia_matrix(self):
@@ -117,7 +170,12 @@ class sim_robot_env:
         J = self.get_trans_jacobian()
         M = self.get_mass_matrix()
         return np.array(np.linalg.inv(np.array(J) @ np.linalg.inv(np.array(M)) @ np.array(J).T))
-
+    def get_inertia_matrix2(self):
+        q = self.get_joint_position2()
+        J = self.get_trans_jacobian2()
+        M = self.get_mass_matrix2()
+        return np.array(np.linalg.inv(np.array(J) @ np.linalg.inv(np.array(M)) @ np.array(J).T))
+    
     def get_stein_divergence_joint_gradient(self, g_current, A_d):
         num_joints = self.numJoints
         dg_dq = np.zeros((num_joints, 1))
@@ -132,9 +190,25 @@ class sim_robot_env:
             g_new = get_stein_divergence(A_new, A_d)
             dg_dq[l] = ((g_new - g_current) / dq)
         return dg_dq
+    def get_stein_divergence_joint_gradient2(self, g_current, A_d):
+        num_joints = self.numJoints
+        dg_dq = np.zeros((num_joints, 1))
+        dq = 0.01
+        q_current = self.get_joint_position2()
+        for l in range(num_joints):
+            q_new = q_current.copy()
+            q_new[l] = q_new[l] + dq
+            jac_t_fn = self.get_trans_jacobian_specific2(q_new)
+            mass_fn = p.calculateMassMatrix(self.robot2, q_new)
+            A_new = np.array(np.linalg.inv(np.array(jac_t_fn) @ np.linalg.inv(np.array(mass_fn)) @ np.array(jac_t_fn).T))
+            g_new = get_stein_divergence(A_new, A_d)
+            dg_dq[l] = ((g_new - g_current) / dq)
+        return dg_dq
 
     def get_ee_velocity_current(self):
         return p.getLinkState(self.robot, self.ee_id, computeLinkVelocity=True)[6]
+    def get_ee_velocity_current2(self):
+        return p.getLinkState(self.robot2, self.ee_id, computeLinkVelocity=True)[6]
 
     def get_directional_inertia_gradient(self, direction):
         num_joints = self.numJoints
@@ -150,12 +224,30 @@ class sim_robot_env:
 
             dL_dq_dir[l] = direction.T @ dL_dq @ direction
         return dL_dq_dir
+    def get_directional_inertia_gradient2(self, direction):
+        num_joints = self.numJoints
+        dL_dq_dir = np.zeros((num_joints, 1))
+        dq = 0.001
+        q_current = self.get_joint_position2()
+        A_current = self.get_inertia_matrix_specific2(q_current)
+        for l in range(num_joints):
+            q_new = q_current.copy()
+            q_new[l] = q_new[l] + dq
+            A_new = self.get_inertia_matrix_specific2(q_new)
+            dL_dq = (A_new - A_current) / dq
+
+            dL_dq_dir[l] = direction.T @ dL_dq @ direction
+        return dL_dq_dir
 
     def get_directional_inertia_gradient_SPD(self, direction):
         return 0
 
     def get_velocity_manipulability_metric(self):
         jac = self.get_trans_jacobian()
+        m = np.sqrt(np.linalg.det(np.array(jac) @ np.array(jac).T))
+        return m
+    def get_velocity_manipulability_metric2(self):
+        jac = self.get_trans_jacobian2()
         m = np.sqrt(np.linalg.det(np.array(jac) @ np.array(jac).T))
         return m
 
@@ -175,9 +267,15 @@ class sim_robot_env:
     def get_collision_points(self):
          p.performCollisionDetection(self.physicsClient)
          return np.array(p.getContactPoints(self.robot, self.box), dtype=object)
+    def get_collision_points2(self):
+         p.performCollisionDetection(self.physicsClient)
+         return np.array(p.getContactPoints(self.robot2, self.box), dtype=object)
 
     def get_collision_position(self):
         collision_points = self.get_collision_points()
+        return collision_points[0][5]
+    def get_collision_position2(self):
+        collision_points = self.get_collision_points2()
         return collision_points[0][5]
 
 
--- simulation_flux_hit.py
+++ simulation_flux_hit.py
@@ -4,6 +4,7 @@ import time
 
 from ds import linear_hitting_ds_pre_impact, linear_ds
 from controller import get_joint_velocities_qp_dir_inertia_specific_NS, get_joint_velocities_qp
+from controller import get_joint_velocities_qp_dir_inertia_specific_NS2, get_joint_velocities_qp2
 from get_robot import sim_robot_env
 from iiwa_environment import object
 from iiwa_environment import physics as phys
@@ -21,10 +22,13 @@ iiwa = sim_robot_env(1, box)
 X_init = [0.3, -0.2, 0.5]
 q_init = iiwa.get_IK_joint_position(X_init) # Currently I am not changing any weights here
 Lambda_init = iiwa.get_inertia_matrix_specific(q_init)
+Lambda_init2 = iiwa.get_inertia_matrix_specific2(q_init)
+
 
 ##################### DS PROPERTIES ####################################
 A = np.array([[-2, 0, 0], [0, -2, 0], [0, 0, -2]])
 h_dir = np.array([0, 1, 0]) # This is the direction of the hitting
+h_dir2 = np.array([0, -1, 0])
 
 ###################### DESIRED DIRECTIONAL PROPERTIES ##################
 box_position_orientation = iiwa.get_box_position_orientation()
@@ -42,9 +46,12 @@ for X_ref in X_ref_grid:
 
     # initialize the robot and the box
     iiwa.set_to_joint_position(q_init)
+    iiwa.set_to_joint_position2(q_init)
     iiwa.reset_box(box_position_init, box_orientation_init)
     lambda_dir = h_dir.T @ Lambda_init @ h_dir
-    is_hit = False
+    lambda_dir2 = h_dir2.T @ Lambda_init2 @ h_dir2
+    is_hit1 = False
+    is_hit2 = False
 
     # take some time
     time.sleep(1)
@@ -56,7 +63,7 @@ for X_ref in X_ref_grid:
     while 1:
         X_qp = np.array(iiwa.get_ee_position())
         
-        if not is_hit:
+        if not is_hit1:
             dX = linear_hitting_ds_pre_impact(A, X_qp, X_ref, h_dir, 0.7, lambda_dir, box.mass)
         else:
             dX = linear_ds(A, X_qp, X_ref)
@@ -71,14 +78,45 @@ for X_ref in X_ref_grid:
         
         iiwa.move_with_joint_velocities(q_dot)
 
-        ## Need something more here later, this is contact detection and getting the contact point
+        
         if(iiwa.get_collision_points().size != 0):
-            is_hit = True
+            is_hit1 = True
             iiwa.get_collision_position()
-
-
+            
         iiwa.step()
         time_now = time.time()
-
-        if(is_hit and iiwa.get_box_speed() < 0.001 and time_now - time_init > contactTime):
-            break
+        
+        if ((time_now - time_init) > 1.5 and iiwa.get_box_speed() < 0.5):
+        #if (iiwa.get_box_position_orientation()[0][1]>0.5):
+            #x_b = np.array(iiwa.get_box_position_orientation()[0]) + np.array([0,0.2,0])
+            while 1:
+                X_qp = np.array(iiwa.get_ee_position2())
+                
+                x_b = np.array(iiwa.get_box_position_orientation()[0])
+                
+                if not is_hit2:
+                    dX = linear_hitting_ds_pre_impact(A, X_qp, x_b, h_dir2, 0.7, lambda_dir2, box.mass)
+                else:
+                    dX = linear_ds(A, X_qp, X_ref)
+    
+                hit_dir = dX / np.linalg.norm(dX)
+    
+                lambda_current = iiwa.get_inertia_matrix2()
+                lambda_dir2 = hit_dir.T @ lambda_current @ hit_dir
+                
+                jac = np.array(iiwa.get_trans_jacobian2())
+                q_dot = get_joint_velocities_qp_dir_inertia_specific_NS2(dX, jac, iiwa, hit_dir, 0.15, lambda_dir2)
+                
+                iiwa.move_with_joint_velocities2(q_dot)
+                
+                ## Need something more here later, this is contact detection and getting the contact point
+                if(iiwa.get_collision_points2().size != 0):
+                    is_hit2 = True
+                    iiwa.get_collision_position2()
+                    
+                if(is_hit1 and is_hit2 and iiwa.get_box_speed() < 0.001 and time_now - time_init > contactTime):
+                    break
+            if(is_hit1 and is_hit2 and iiwa.get_box_speed() < 0.001 and time_now - time_init > contactTime):
+                break
+        
+        
