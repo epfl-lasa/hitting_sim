367569d04209f20eaac56f2822a6536042f5b49b
hkhurana <harshit.khurana@epfl.ch>
2023-03-14 10:44:11 +0100

    change of path

--- get_robot.py
+++ get_robot.py
@@ -26,10 +26,10 @@ class sim_robot_env:
         Create a scene too
         the environment with the box and table for the different scenarios I would like in the simulation
         '''
-        self.box = p.loadURDF("../descriptions/robot_descriptions/objects_description/objects/simple_box.urdf",
+        self.box = p.loadURDF("descriptions/robot_descriptions/objects_description/objects/simple_box.urdf",
                               [0.5, 0.3, 0.5], globalScaling=1.0, useFixedBase=0)
         tableOrientation = p.getQuaternionFromEuler([0, 0, math.pi / 2])
-        self.table = p.loadURDF("../descriptions/robot_descriptions/objects_description/objects/table.urdf",
+        self.table = p.loadURDF("descriptions/robot_descriptions/objects_description/objects/table.urdf",
                            [1.15, 0.45, 0.0], tableOrientation, globalScaling=1.0, useFixedBase=1)
         p.changeDynamics(self.box, -1, mass=box_object.mass, linearDamping=0.04, angularDamping=0.04, rollingFriction=0.01,
                          spinningFriction=0.02, restitution=0, lateralFriction=0.1)
