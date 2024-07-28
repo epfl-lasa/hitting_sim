a36a9ef26ba2526da21f512eca4008bf59573b60
harshitk95 <harshit.khurana@epfl.ch>
2024-07-28 10:52:36 +0200

    joint classification being added

--- joint_classification.py
+++ joint_classification.py
@@ -10,17 +10,26 @@ from get_robot_iiwa import sim_robot
 from iiwa_environment import object
 from iiwa_environment import physics as phys
 
+import pickle
 
-X_des = np.array([0.3, 0.3, 0.3])
 
-robot = sim_robot(0, 1)
+############################### Import the gmm model ##############################
+gmm_5 = pickle.load(open("gmm_models/gmm_5", "rb"))
+gmm_6 = pickle.load(open("gmm_models/gmm_6", "rb"))
 
-num_joints = 7
 
-###################################################
 
-v_dir = np.array([0, 1, 0])
+X_des = np.array([0.5, 0.4, 0.2])
+
+# robot = sim_robot(0, 1)
+
+# num_joints = 7
+
+# ###################################################
+
+# v_dir = np.array([0, 1, 0])
 
 grid = 100
 
 m_obj = np.linspace(0.1, 5, grid)
+print(m_obj)
